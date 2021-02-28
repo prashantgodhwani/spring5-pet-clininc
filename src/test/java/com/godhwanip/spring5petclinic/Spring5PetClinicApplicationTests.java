e97174c5984d275d0398daad88b3146bdbc136af
Prashant Godhwani <prashant.godhwani@hotmail.com>
2021-02-28 12:53:02 +0530

    Initial Project Add

---
+++ src/test/java/com/godhwanip/spring5petclinic/Spring5PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godhwanip.spring5petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
